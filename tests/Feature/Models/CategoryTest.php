4c02cabbc03727d43bd842fa6815b4e2cf2d7b17
mysticsadnesh <sandesh@mysticastra.com>
2025-01-24 15:37:57 +0545

    [ADD]: added model test cases for all models

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Enums\CategoryStatus;
+use App\Models\Category;
+use App\Models\Post;
+
+beforeEach(function () {
+    $this->category = Category::factory()->hasPosts(2)->create();
+});
+
+test('to array', function () {
+    expect(array_keys($this->category->toArray()))->toBe([
+        'name',
+        'slug',
+        'description',
+        'status',
+        'updated_at',
+        'created_at',
+        'id'
+    ]);
+});
+
+test('casts', function () {
+    expect($this->category->status)->toBeInstanceOf(CategoryStatus::class);
+});
+
+test('relations', function () {
+    expect($this->category->posts)->toHaveCount(2)->each->toBeInstanceOf(Post::class);
+});
